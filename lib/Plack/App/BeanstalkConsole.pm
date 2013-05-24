dccd4a2f45352d40a99c005ddcee3a1f5ab475dc
Karen Etheridge <ether@cpan.org>
2013-05-24 13:47:01 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.006, github profile.

---
+++ lib/Plack/App/BeanstalkConsole.pm
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+package Plack::App::BeanstalkConsole;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS/METHODS
+
+=begin :list
+
+* C<foo>
+
+=end :list
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Plack-App-BeanstalkConsole>
+(or L<bug-Plack-App-BeanstalkConsole@rt.cpan.org|mailto:bug-Plack-App-BeanstalkConsole@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut
