421157b2df4d72176526f2b09724497650717379
Karen Etheridge <ether@cpan.org>
2016-09-17 14:05:53 -0700

    ensure vim modeline is in the first 5 line

--- lib/Plack/App/BeanstalkConsole.pm
+++ lib/Plack/App/BeanstalkConsole.pm
@@ -1,9 +1,9 @@
 use strict;
 use warnings;
 package Plack::App::BeanstalkConsole;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 # ABSTRACT: A web application that provides access to Beanstalk statistics and tools
 # KEYWORDS: beanstalk jobs queue web application dashboard console plack psgi
-# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.010';
 
